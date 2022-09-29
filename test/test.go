65a2f4ec80b74a40ddf8fa3e7d13b246ddc05e92
brianmikht <110759186+brianmikht@users.noreply.github.com>
2022-09-29 18:54:46 +0700

    test mock

--- test/test.go
+++ test/test.go
@@ -1,1 +1,14 @@
 package test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Sebelum Unit Test")
+
+	m.Run() //eksekusi semua unit test
+
+	fmt.Println("Setelah Unit Test")
+}
